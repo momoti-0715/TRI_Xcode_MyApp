c7fac739ae2bc23e6f6b0e0ca4f6913cb3924769
momoti-0715 <ayapon2003@outlook.jp>
2026-07-08 13:50:25 +0900

    Initial Commit

---
+++ MyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyApp
+//
+//  Created by nv01 on 2026/07/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
